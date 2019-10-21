1c5fdc85b4c2041032d7ca4b5f0213d5e4da83f6
percy1998 <percy3227@yahoo.co.jp>
2019-10-21 08:03:10 +0000

    create user-signin & user-signout

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,8 +3,31 @@ class SessionsController < ApplicationController
   end
 
   def create
+    email = params[:session][:email].downcase
+    password = params[:session][:password]
+    if signin(email, password)
+      flash[:success] = 'サインインに成功しました。'
+      redirect_to @user
+    else
+      flash.now[:danger] = 'サインインに失敗しました。'
+      render 'new'
+    end
   end
 
   def destroy
+    session[:user_id] = nil
+    flash[:success] = 'サインアウトしました。'
+    redirect_to root_url
+  end
+  
+  private 
+  def signin(email, password)
+    @user = User.find_by(email: email)
+    if @user && @user.authenticate(password)
+      session[:user_id] = @user.id
+      return true
+    else
+      return false
+    end
   end
 end
