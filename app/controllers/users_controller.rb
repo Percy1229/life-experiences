5237926d316d4a4a4fb538022821d99d2b398f8f
percy1998 <percy3227@yahoo.co.jp>
2019-10-21 05:45:11 +0000

    user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,8 +7,23 @@ class UsersController < ApplicationController
   end
 
   def new
+    @user = User.new
   end
 
   def create
+    @user = User.new(user_params)
+    
+    if @user.save
+      flash[:success] = 'ユーザーを登録しました。'
+      redirect_to toppage_url
+    else
+      falsh.now[:danger] = 'ユーザーの登録に失敗しました。'
+      render :new
+    end 
+  end
+  
+  private
+  def user_params 
+    params.require(:user).permit(:name, :email, :password, :password_confirmation, :introduce, :image)
   end
 end
